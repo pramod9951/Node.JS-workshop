cea6a3535f5ef90eed8deda105f05375123fc42e
pramod9951 <87662603+pramod9951@users.noreply.github.com>
2022-09-08 11:51:53 +0530

    Add files via upload

---
+++ if.js
@@ -0,0 +1,8 @@
+if(c==30)
+{
+    console.log( "sum is 30");
+}
+else
+{
+    console.log("ELSE");
+}
---
+++ sum.js
@@ -0,0 +1,17 @@
+console.log("welocome to pramod lecture");
+var a =10;
+var b =20;
+var c = a + b;
+console.log("A value is " + a +" and b value is " +b);
+//ES6
+console.log(`A value is ${a} and B value is ${b} 
+sum is ${a+b}`);
+
+if(c==30)
+{
+    console.log( "sum is 30");
+}
+else
+{
+    console.log("ELSE");
+}
